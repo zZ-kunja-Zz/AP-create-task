3ab6a05a40f6990f04c876ad1e79785a4197e8da
kunja <73044225+zZ-kunja-Zz@users.noreply.github.com>
2021-02-25 09:56:44 -0800

    started the 8/9 thing

--- solve/oneMissing.h
+++ solve/oneMissing.h
@@ -5,4 +5,14 @@ using namespace std;
 void oneMissing(int *board[9][9]) {
 	int h, v;
 	int i, j;
+	int zeroCountHor[9];
+	int zeroCountVer[9];
+	int zeroCountBox[9];
+	for (v = 0; v < 9; v++) {
+		for (h = 0; h < 9; h++) {
+			if (board[v][h] == 0) {
+
+		}
+	}
 }
+// the way to do this is to get the 8 numbers and subtract them fron the sum of 1-9 im literally a genius 
