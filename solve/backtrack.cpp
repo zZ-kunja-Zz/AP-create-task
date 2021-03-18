9a376b04c58a4ccfcf159af5e99f94d67e932a8c
kunja <73044225+zZ-kunja-Zz@users.noreply.github.com>
2021-03-17 23:20:11 -0700

    solving works

--- solve/backtrack.cpp
+++ solve/backtrack.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-int isLegal(int** board) {
+void wipeBoard(int** board, int** board2) {
+	int v, h;
+	for (v = 0; v < 9; v++) {
+		for (h = 0; h < 9; h++) {
+			board2[v][h] = board2[v][h];
+		}
+	}
+}
+
+int isLegal(int** board, int** board01) {
 	int h, v;
 	int i, j;
+	int test;
 	int listForTest[9];
 	for (v = 0; v < 9; v++) {
 		for (i = 0; i < 9; i++) {
@@ -12,7 +23,9 @@ int isLegal(int** board) {
 		}
 		for (h = 0; h < 9; h++) {
 			if (board[v][h] != 0) {
-				listForTest[--board[v][h]]++;
+				test = board[v][h];
+				cout << test << endl;
+				listForTest[--test]++;
 			}
 		}
 		for (i = 0; i < 9; i++) {
@@ -60,12 +73,59 @@ int isLegal(int** board) {
 	return 1;
 }
 
-void backtrack(int** board) {
-	int h, v;
+void backtrack(int** board, int** board01, int** board2) {
+	cout << "bruh" << endl;
+	int v, h;
+	int i, j;
+	int counter = 0;
+	int emptyTiles[81];
+	int solved = 0;
+	int temp;
+	
 	for (v = 0; v < 9; v++) {
 		for (h = 0; h < 9; h++) {
-			if (board[v][h] == 0)
-
+			if (board[v][h] == 0) {
+				board01[v][h] = 0;
+			}
+			else {
+				board01[v][h] = 1;
+			}
 		}
 	}
+	for (i = 0; i < 81; i++) {
+		if (board01[i / 9][i % 9] == 0) {
+			emptyTiles[counter] = i;
+			counter++;
+		}
+	}
+	int current = 0;
+	while (solved != 1) {
+		cout << "test" << endl;
+		if (current == counter) {
+			solved = 1;
+			break;
+		}
+		temp = emptyTiles[current];
+		if (board2[temp / 9][temp % 9] == 9) {
+			board2[temp / 9][temp % 9] = 0;
+			current--;
+			continue;
+		}
+		board2[current / 9][current % 9]++;
+		if (isLegal(board2, board01)) {
+				current++;
+		}
+		for (i = 0; i < 9; i++) {
+			for (j = 0; j < 9; j++) {
+				cout << board[i][j] << ' ';
+			}
+			cout << endl;
+		}
+	}
+	for (v = 0; v < 9; v++) {
+		for (h = 0; h < 9; h++) {
+			board[v][h] = board2[v][h];
+		}
+	}
+
 }
--- solve/main.cpp
+++ solve/main.cpp
@@ -9,11 +9,139 @@
 #include <fstream> //file stuffs
 #include <string>
 #include <sstream>
-#include "optimalNumber.h"
-#include "oneMissing.h"
-#include "rowFindThing.h"
+
 
 using namespace std;
+
+int isLegal(int** board, int** board01) {
+	int h, v;
+	int i, j;
+	int test;
+	int listForTest[9];
+	for (v = 0; v < 9; v++) {
+		for (i = 0; i < 9; i++) {
+			listForTest[i] = 0;
+		}
+		for (h = 0; h < 9; h++) {
+			if (board[v][h] != 0) {
+				test = board[v][h];
+				listForTest[--test]++;
+			}
+		}
+		for (i = 0; i < 9; i++) {
+			if (listForTest[i] > 1) {
+				return 0;
+			}
+		}
+
+	}
+	for (h = 0; h < 9; h++) {
+		for (i = 0; i < 9; i++) {
+			listForTest[i] = 0;
+		}
+		for (v = 0; v < 9; v++) {
+			if (board[v][h] != 0) {
+				test = board[v][h];
+				listForTest[--test]++;
+			}
+		}
+		for (i = 0; i < 9; i++) {
+			if (listForTest[i] > 1) {
+				return 0;
+			}
+		}
+	}
+	for (v = 0; v < 3; v++) {
+		for (h = 0; h < 3; h++) {
+			for (i = 0; i < 9; i++) {
+				listForTest[i] = 0;
+			}
+			for (i = 0; i < 3; i++) {
+				for (j = 0; j < 3; j++) {
+					if (board[3 * v + i][3 * h + j] != 0) {
+						test = board[3 * v + i][3 * h + j];
+						listForTest[--test]++;
+					}
+				}
+			}
+			for (i = 0; i < 9; i++) {
+				if (listForTest[i] > 1) {
+					return 0;
+				}
+			}
+		}
+
+	}
+	return 1;
+}
+
+void backtrack(int** board, int** board01, int** board2) {
+	int v, h;
+	int i, j;
+	int counter = 0;
+	int emptyTiles[81];
+	int solved = 0;
+	int temp;
+
+	for (v = 0; v < 9; v++) {
+		for (h = 0; h < 9; h++) {
+			if (board[v][h] == 0) {
+				board01[v][h] = 0;
+			}
+			else {
+				board01[v][h] = 1;
+			}
+		}
+	}
+	for (v = 0; v < 9; v++) {
+		for (h = 0; h < 9; h++) {
+			board2[v][h] = board[v][h];
+		}
+	}
+	for (i = 0; i < 81; i++) {
+		if (board01[i / 9][i % 9] == 0) {
+			emptyTiles[counter] = i;
+			counter++;
+		}
+	}
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			cout << board2[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	cout << endl;
+	int current = 0;
+	while (solved != 1) {
+		if (current == counter) {
+			solved = 1;
+			break;
+		}
+		temp = emptyTiles[current];
+		if (board2[temp / 9][temp % 9] == 9) {
+			board2[temp / 9][temp % 9] = 0;
+			current--;
+			continue;
+		}
+		board2[temp / 9][temp % 9]++;
+		if (isLegal(board2, board01)) {
+			current++;
+		}
+		for (i = 0; i < 9; i++) {
+			for (j = 0; j < 9; j++) {
+				cout << board2[i][j] << ' ';
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	for (v = 0; v < 9; v++) {
+		for (h = 0; h < 9; h++) {
+			board[v][h] = board2[v][h];
+		}
+	}
+
+}
 
 
 int main() {
@@ -21,8 +149,18 @@ int main() {
 	string line;
 	int** board;
 	board = new int* [10];
-	for (int i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10; i++) {
 		board[i] = new int[10];
+	}
+	int** board01;
+	board01 = new int* [10];
+	for (int i = 0; i < 10; i++) {
+		board01[i] = new int[10];
+	}
+	int** board2;
+	board2 = new int* [10];
+	for (int i = 0; i < 10; i++) {
+		board2[i] = new int[10];
 	}
 	int i, j = 0;
 	int temp;
@@ -30,7 +168,7 @@ int main() {
 	int opNum;
 	int timer = 0;
 	int finished = 0;
-	fin.open("sudoku.txt");
+	fin.open("sudoku1.txt");
 	while (getline(fin, line)) {
 		stringstream linestream(line);
 		string data;
@@ -52,52 +190,16 @@ int main() {
 	*/
 	fin.close();
 
-	//cout << optimalNumber(board, &z) << endl;
-	//rowFindThing(1, board);
-	/*
-	while (finished == 0 && timer < 100) {
-		timer++;
-		optimalNumber(board, repList);
-		for (i = 0; i < 9; i++) {
-			oneMissing(board);
-			switch (repList[i]) {
-			case 1:
-				finished = 0;
-				break;
-			case 2:
-				finished = 0;
-				break;
-			case 3:
-				finished = 0;
-				break;
-			case 4:
-				finished = 0;
-				rowFindThing(repList[i], board);
-				break;
-			case 5:
-				finished = 0;
-				rowFindThing(repList[i], board);
-				break;
-			case 6:
-				finished = 0;
-				rowFindThing(repList[i], board);
-				break;
-			case 7:
-				finished = 0;
-				rowFindThing(repList[i], board);
-				break;
-			case 8:
-				finished = 0;
-				rowFindThing(repList[i], board);
-				break;
-			case 9:
-				finished = 1;
-				break;
-
-			}
+	
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			cout << board[i][j] << ' ';
 		}
+		cout << endl;
 	}
-	*/
+	cout << endl;
+	
+	backtrack(board, board01, board2);
 
 	for (i = 0; i < 9; i++) {
 		for (j = 0; j < 9; j++) {
